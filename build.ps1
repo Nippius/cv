9aed69ee1d23d7649073f89bd291e3f37f3888c2
Tiago Costa <nippius+github@gmail.com>
2017-09-19 17:05:37 +0100

    Add initial CV

---
+++ build.ps1
@@ -0,0 +1,68 @@
+﻿##
+# Misc
+##
+$FILENAME_PREFIX = "Tiago_Costa_"
+$BASE_LOCATION = Get-Location
+$OUTPUT_LOCATION = "$BASE_LOCATION\output"
+
+##
+# Xelatex configuration
+##
+$TEX_LOCATION = $env:TEX
+$XELATEX = "$TEX_LOCATION\xelatex.exe"
+$XELATEX_FLAGS = "-output-directory","build"
+
+##
+# Files and folders to build
+##
+$FOLDERS = "en", "pt"
+$FILES = "cv"
+
+function moveFiles()
+{
+    If((Test-Path "$OUTPUT_LOCATION" -pathType container) -eq $false)
+    {
+        New-Item $OUTPUT_LOCATION -type directory | Out-Null
+    }
+
+    ForEach($folder in $FOLDERS)
+    {
+        ForEach($file in $FILES)
+        {
+            $origin = $folder+"\build\"+$file+".pdf"
+            $destination = $OUTPUT_LOCATION+"\"+$FILENAME_PREFIX+$file.ToUpperInvariant()+"_"+$folder.ToUpperInvariant()+".pdf"
+            Move-Item $origin $destination -Force
+        }
+    }
+}
+
+function buildFiles()
+{
+    ForEach($folder in $FOLDERS)
+    {
+        # Needed for xelatex to output to the correct folder.
+		Set-Location $BASE_LOCATION\$folder
+
+		If((Test-Path "build" -pathType container) -eq $false)
+        {
+            New-Item "build" -type directory | Out-Null
+        }
+
+		ForEach($file in $files){
+			Write-Host -NoNewLine "Building $folder\$file... "
+			& $XELATEX $XELATEX_FLAGS $file 2>&1 > build\build.log;
+			Write-Output "Done!"
+		}
+
+        Set-Location $BASE_LOCATION
+    }
+}
+
+try
+{
+    buildFiles;
+    moveFiles;
+}
+finally{
+	Set-Location $BASE_LOCATION
+}
---
+++ clean.ps1
@@ -0,0 +1,36 @@
+﻿# Grab initial location so that we can return here at the end.
+$BASE_LOCATION = Get-Location
+
+$FOLDERS = "en", "pt"
+
+function cleanup()
+{
+	# Clean build output
+	ForEach($folder in $FOLDERS)
+	{
+        $folderToRemove = "$BASE_LOCATION\$folder\build";
+        If((Test-Path $folderToRemove -pathType container))
+        {
+		    Remove-Item $folderToRemove -Recurse
+            Write-Output "Cleaned: $folderToRemove"
+        }
+	}
+	
+	# Clean output
+    $outputFolder = "$BASE_LOCATION\output"
+    If((Test-Path $outputFolder -pathType container))
+    {
+        Remove-Item $outputFolder -Recurse
+        Write-Output "Cleaned: $outputFolder"
+    }
+}
+
+try
+{
+    cleanup;
+}
+finally{
+	Set-Location $BASE_LOCATION
+}
+
+
