c589555f3087b42f73c404ac4eede1f85d04bb0d
Nippius <Nippius@users.noreply.github.com>
2026-01-08 16:53:39 +0000

    Upda path to latest TeXLive version

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ $OUTPUT_LOCATION = "$BASE_LOCATION\output"
 ##
 # Xelatex configuration
 ##
-$TEX_LOCATION = Join-Path -Path $env:TEXDIR -ChildPath '2021' 'bin' 'win32'
+$TEX_LOCATION = Join-Path -Path $env:TEXDIR -ChildPath '2025' 'bin' 'windows'
 $XELATEX = "$TEX_LOCATION\xelatex.exe"
 $XELATEX_FLAGS = "-output-directory","build"
 
