715ddbd59b304faea4f2272b397efcdbff7f8d5f
Nippius <Nippius@users.noreply.github.com>
2026-01-10 03:44:22 +0000

    Cleanup build script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
 ﻿$BASE_LOCATION = $PSScriptRoot
+$BUILD_FOLDER = Join-Path $BASE_LOCATION "build"
+$OUT_FOLDER = Join-Path $BASE_LOCATION "out"
 $TEX_LOCATION = Join-Path -Path $env:TEXDIR -ChildPath '2025' 'bin' 'windows'
 $XELATEX = "$TEX_LOCATION\xelatex.exe"
 $XELATEX_FLAGS = "-output-directory", "build"
-$BUILD_FOLDER = Join-Path $BASE_LOCATION "build"
-$OUT_FOLDER = Join-Path $BASE_LOCATION "out"
 
 
 if ((Test-Path $BUILD_FOLDER -pathType container) -eq $false) {
@@ -16,10 +16,9 @@ if ((Test-Path $OUT_FOLDER -pathType container) -eq $false) {
 $texFiles = Get-ChildItem -Recurse -Filter "*.tex"
 foreach ($file in $texFiles) {
     Write-Host -NoNewLine "Building $file... "
-    & $XELATEX $XELATEX_FLAGS $file 2>&1 > (Join-Path $BUILD_FOLDER "build.log")
+    & $XELATEX $XELATEX_FLAGS $file 2>&1 >> (Join-Path $BUILD_FOLDER "build.log")
 
-    $language = $file.Directory.Name
-    Rename-Item -Path (Join-Path $BUILD_FOLDER "$($file.BaseName).pdf") -NewName "Tiago_Costa_CV_$($language.ToUpper()).pdf"
+    Rename-Item -Path (Join-Path $BUILD_FOLDER "$($file.BaseName).pdf") -NewName "Tiago_Costa_CV_$($file.BaseName.ToUpper()).pdf"
     Write-Output "Done!"
 }
 
