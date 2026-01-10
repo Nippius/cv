9d3529a0d57001c3261b9687a10514c347c01e00
Nippius <Nippius@users.noreply.github.com>
2026-01-10 03:39:51 +0000

    Simplify build script

--- build.ps1
+++ build.ps1
@@ -1,68 +1,26 @@
-﻿##
-# Misc
-##
-$FILENAME_PREFIX = "Tiago_Costa_"
-$BASE_LOCATION = Get-Location
-$OUTPUT_LOCATION = "$BASE_LOCATION\output"
-
-##
-# Xelatex configuration
-##
+﻿$BASE_LOCATION = $PSScriptRoot
 $TEX_LOCATION = Join-Path -Path $env:TEXDIR -ChildPath '2025' 'bin' 'windows'
 $XELATEX = "$TEX_LOCATION\xelatex.exe"
-$XELATEX_FLAGS = "-output-directory","build"
+$XELATEX_FLAGS = "-output-directory", "build"
+$BUILD_FOLDER = Join-Path $BASE_LOCATION "build"
+$OUT_FOLDER = Join-Path $BASE_LOCATION "out"
 
-##
-# Files and folders to build
-##
-$FOLDERS = "en", "pt"
-$FILES = "cv"
 
-function moveFiles()
-{
-    If((Test-Path "$OUTPUT_LOCATION" -pathType container) -eq $false)
-    {
-        New-Item $OUTPUT_LOCATION -type directory | Out-Null
-    }
-
-    ForEach($folder in $FOLDERS)
-    {
-        ForEach($file in $FILES)
-        {
-            $origin = $folder+"\build\"+$file+".pdf"
-            $destination = $OUTPUT_LOCATION+"\"+$FILENAME_PREFIX+$file.ToUpperInvariant()+"_"+$folder.ToUpperInvariant()+".pdf"
-            Move-Item $origin $destination -Force
-        }
-    }
+if ((Test-Path $BUILD_FOLDER -pathType container) -eq $false) {
+    New-Item $BUILD_FOLDER -type directory | Out-Null
+}
+if ((Test-Path $OUT_FOLDER -pathType container) -eq $false) {
+    New-Item $OUT_FOLDER -type directory | Out-Null
 }
 
-function buildFiles()
-{
-    ForEach($folder in $FOLDERS)
-    {
-        # Needed for xelatex to output to the correct folder.
-		Set-Location $BASE_LOCATION\$folder
+$texFiles = Get-ChildItem -Recurse -Filter "*.tex"
+foreach ($file in $texFiles) {
+    Write-Host -NoNewLine "Building $file... "
+    & $XELATEX $XELATEX_FLAGS $file 2>&1 > (Join-Path $BUILD_FOLDER "build.log")
 
-		If((Test-Path "build" -pathType container) -eq $false)
-        {
-            New-Item "build" -type directory | Out-Null
-        }
-
-		ForEach($file in $files){
-			Write-Host -NoNewLine "Building $folder\$file... "
-			& $XELATEX $XELATEX_FLAGS $file 2>&1 > build\build.log;
-			Write-Output "Done!"
-		}
-
-        Set-Location $BASE_LOCATION
-    }
+    $language = $file.Directory.Name
+    Rename-Item -Path (Join-Path $BUILD_FOLDER "$($file.BaseName).pdf") -NewName "Tiago_Costa_CV_$($language.ToUpper()).pdf"
+    Write-Output "Done!"
 }
 
-try
-{
-    buildFiles;
-    moveFiles;
-}
-finally{
-	Set-Location $BASE_LOCATION
-}
+Get-ChildItem -Recurse -Filter "*.pdf" | Move-Item -Destination $OUT_FOLDER -Force
