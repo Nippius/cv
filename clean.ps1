ded51c648f31d7136c5ce4c463141a7d34318399
Nippius <Nippius@users.noreply.github.com>
2026-01-10 03:44:37 +0000

    Simplify clean script

--- clean.ps1
+++ clean.ps1
@@ -1,36 +1,6 @@
-﻿# Grab initial location so that we can return here at the end.
-$BASE_LOCATION = Get-Location
+﻿$BASE_LOCATION = $PSScriptRoot
+$BUILD_FOLDER = Join-Path $BASE_LOCATION "build"
+$OUT_FOLDER = Join-Path $BASE_LOCATION "out"
 
-$FOLDERS = "en", "pt"
-
-function cleanup()
-{
-	# Clean build output
-	ForEach($folder in $FOLDERS)
-	{
-        $folderToRemove = "$BASE_LOCATION\$folder\build";
-        If((Test-Path $folderToRemove -pathType container))
-        {
-		    Remove-Item $folderToRemove -Recurse
-            Write-Output "Cleaned: $folderToRemove"
-        }
-	}
-	
-	# Clean output
-    $outputFolder = "$BASE_LOCATION\output"
-    If((Test-Path $outputFolder -pathType container))
-    {
-        Remove-Item $outputFolder -Recurse
-        Write-Output "Cleaned: $outputFolder"
-    }
-}
-
-try
-{
-    cleanup;
-}
-finally{
-	Set-Location $BASE_LOCATION
-}
-
-
+Remove-Item $BUILD_FOLDER -Recurse
+Remove-Item $OUT_FOLDER -Recurse
